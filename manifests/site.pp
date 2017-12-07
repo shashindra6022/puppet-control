94cfe8388e8895544b608e8c2cbae4e029345b61
root <root@shashiudawa5.mylabserver.com>
2017-12-07 06:47:35 +0000

    test4

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,7 +25,7 @@ File { backup => false }
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 node 'shashiudawa2.mylabserver.com' {
- class { '::role::linux': }
+ class { 'role::linux': }
 }
 
 node default {
